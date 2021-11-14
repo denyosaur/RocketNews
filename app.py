8c1dad0df3db9636cc1248c56eb648233ba29da3
daniel kim <danielkim@daniels-MacBook-Pro.local>
2021-11-14 15:55:05 -0800

    psycopg2 sqlalchemy fix

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify, g, session, redirect, flash
 from time import sleep
-from functions import Coins, News #, Stocks
+from functions import Coins, News
 from models import db, User, CoinFollowed, CoinsList, connect_db
-#from models import db, User, CoinFollowed, StockFollowed, StocksList, CoinsList, connect_db
 from form import UserRegistration, UserProfile, LoginForm
 from sqlalchemy.exc import IntegrityError
 
@@ -11,7 +10,7 @@ import os
 from flask_debugtoolbar import DebugToolbarExtension
 #########################################################################################################
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///rocket_news')) #.replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///rocket_news')).replace("://", "ql://", 1)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
@@ -28,9 +27,7 @@ cmc_api_key2 = '500230bd-97f1-4df6-8369-cef1446e0cb3'
 
 #########################################################################################################
 coins = Coins()
-#stocks = Stocks()
 news = News()
-# stocklist = StocksList()
 coinlist = CoinsList()
 user= User()
 
@@ -129,13 +126,8 @@ def logged_in_homepage():
 
     user_info = g.user
     user_coins = g.user.coins
-
-    # followed_stocks = StockFollowed.query.filter_by(user_id=user_info.id)
-
     followed_coins_symbol = coins.symbols_string(user_coins)
     followed_coins = coins.explore_coin_info(followed_coins_symbol)
-
-    # followed_stocks_info = stocks.followed_stock_info(followed_stocks)
 
     return render_template('users-homepage.html', user=user_info, followed_coins=followed_coins) #, followed_stocks=followed_stocks_info)
 
@@ -209,29 +201,6 @@ def add_followed_coin(symbol):
             'Message':'Successfully Added'
             }), 204)
 
-# @app.route('/add/stock/<symbol>', methods=['POST'])
-# def add_followed_stock(symbol):
-#     '''App route for adding stocks to a user's coin follow list
-#     if the stock is already on the followed list, remove it from the list
-#     this route returns a JSON confirming creation or deletion of the followed stocks.'''
-#     if not g.user:
-#         return redirect('/login')
-
-#     stock = StocksList.query.filter_by(stock_symbol=symbol).first()
-#     followed_stock = StockFollowed.query.filter_by(stock_id=stock.stock_symbol).first()
-#     if followed_stock:
-#         StockFollowed.query.filter_by(stock_id=stock.stock_symbol).delete()
-#         db.session.commit()
-#         return (jsonify(info={
-#             'Message':'Successfully Deleted'
-#             }), 204)
-#     else:
-#         new_stock_followed = StockFollowed.follow_stock(g.user.id, stock.stock_symbol)
-#         db.session.commit()
-#         return (jsonify(info={
-#             'Message':'Successfully Added'
-#             }), 204)
-    
 @app.route('/coins/scroll/<int:page>')
 def get_more_coins(page):
     '''app route for exploring all coins
@@ -252,14 +221,6 @@ def search_coin_db_similar(input):
     query_obj = coins.coin_query_serialize(queried)
     return jsonify(results=query_obj)
 
-# @app.route('/api/stock-search/<input>')
-# def search_stock_db_similar(input):
-#     '''this route is used to search for stocks in the database.
-#     route is used for search bar to find stocks according to the users text inputs'''
-#     queried = stocklist.query.filter_by(stock_symbol=input).limit(10).all()
-#     query_obj = stocks.stock_query_serialize(queried)
-#     return jsonify(results=query_obj)
-
 @app.route('/api/update-coin-list', methods=['GET', 'POST'])
 def api_coin_list():
     '''NOT USABLE BY USERS - for back end updating DB only
@@ -271,22 +232,6 @@ def api_coin_list():
     
     db.session.commit()
     return render_template('index.html')
-
-# @app.route('/api/update-stock-list', methods=['GET', 'POST'])
-# def api_stock_list():
-#     '''NOT USABLE BY USERS - for back end updating DB only
-#     this route updates the stocks database. 
-#     Polygon.io only returns 50 stocks out of 40112. Route will call the API 803 times with a 15 second delay (Polygon only allows 5 calls/minute)'''
-#     for page_num in range(803):
-#         sleep(15)
-#         ticker_map = stocks.stock_mapping(page_num)
-
-#         for ticker in ticker_map:
-#             ticker_info = stocks.push_stock_to_db(ticker)
-    
-#     db.session.commit()
-#     return render_template('index.html')
-
 
 @app.route('/api/coin-news/<coin_symbol>/<int:count>', methods=['GET'])
 def newsapi_articles(coin_symbol, count):
@@ -300,17 +245,6 @@ def newsapi_articles(coin_symbol, count):
     serialized_articles = [news.serialize_articles(art) for art in news_articles]
 
     return jsonify(articles=serialized_articles)
-
-# @app.route('/api/stock-news/<stock_symbol>/<int:count>', methods=['GET'])
-# def stockapi_articles(stock_symbol, count):
-#     '''app route for getting articles from News API. these articles are used for infinite scrolling
-#     using a backend route to GET news articles so that front end doesn't have to
-#     everytime this route is called, increase polygon counter by one
-#     counter is reset on page refresh'''
-#     news_articles = news.get_stock_articles(stock_symbol, count)
-
-#     return jsonify(articles=news_articles)
-
 
 #########################################################################################################
 #########################################################################################################
@@ -347,37 +281,3 @@ def coin_info(coin_symbol):
         return render_template('coin-info.html', coin=coin_info, coin_history=coin_history, news_articles=news_articles, followed=followed_ids_list)
 
     return render_template('coin-info.html', coin=coin_info, coin_history=jsonify(coin_history), news_articles=news_articles)
-
-# @app.route('/stocks')
-# def stock_list():
-#     '''app route for exploring all stock
-#     if user is logged in, this route will also show which stock are being followed by markeitng them with different icon'''
-#     stocks_list = stocklist.query.all()
-
-#     if g.user:
-#         stock_followed = StockFollowed.query.with_entities(StockFollowed.stock_id).filter_by(user_id=g.user.id).all()
-#         followed_ids_list = [item[0] for item in stock_followed]
-#         return render_template('stocks-list.html', stocks=stocks_list, stock_followed=followed_ids_list)
-
-#     return render_template('stocks-list.html', stocks=stocks_list)
-
-# @app.route('/stocks/<stock_symbol>')
-# def stock_info(stock_symbol):
-#     '''app route for viewing a specific stocks information, previous daily info, and news
-#     - ticker_info - grab ticker daily performance
-#     - stock_history - get previous 60day performance for stock
-#     - company_info - grab information about company's information
-#     - if user is logged in, pass in information on which coins were followed'''
-#     ticker_info = stocks.explore_stock_info(stock_symbol).get('results')[0]
-#     stock_history = stocks.explore_stock_history(stock_symbol)
-#     company_info = stocks.explore_company_info(stock_symbol)
-
-#     news_articles = news.get_stock_articles(stock_symbol, 1)
-
-#     if g.user:
-#         followed_ids = StockFollowed.query.with_entities(StockFollowed.stock_id).filter_by(user_id=g.user.id).all()
-#         followed_ids_list = [item[0] for item in followed_ids]
-#         return render_template('stocks-info.html', stock=ticker_info, stock_history=stock_history, company=company_info, news_articles=news_articles, followed=followed_ids_list)
-
-#     return render_template('stocks-info.html', stock=ticker_info, stock_history=stock_history, news_articles=news_articles, company=company_info)
-
