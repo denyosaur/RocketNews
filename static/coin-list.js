736d8186d099d9c906dffd700f3432d874b4e1f7
denyosaur <operationdaniel@gmail.com>
2022-01-04 15:24:38 -0600

    refactored news articles javascript

--- static/coin-list.js
+++ static/coin-list.js
@@ -1,6 +1,6 @@
-    /** helpers to handle infinite scrolling for coins 
-     * OOP class of helpers used to make GET request and add onto the current coins HTML list
-     */
+/** helpers to handle infinite scrolling for coins 
+ * OOP class of helpers used to make GET request and add onto the current coins HTML list
+ */
 
 class CoinListHelpers {
     constructor () {
@@ -14,9 +14,17 @@ class CoinListHelpers {
     async getMoreCoins() {
         const res = await axios.get(`/coins/scroll/${this.scrollCount}`); //back end API GET request
         let newCoins = res.data.data; //holds new coins results
-        let followed = new Set(res.data.followed); //holds information on which coins are followed by user
+        let followed = new Set(res.data.followed.split(",")); //holds information on which coins are followed by user
         this.addCoinsToHtml(newCoins, followed); //function to add coin results to HTML
         this.ajaxCoinsLock = false; //set ajaxCoinsLock to false
+    }
+
+    /** functions for adding coin to favorite table
+    * sends a POST request to backend api for adding or removing coin
+    */
+    async addCoinToFavorite(symbol) {
+        const res = await axios.post(`/add/coin/${symbol}`);
+        return res;
     }
 
     /** Function to add coins resuls to HTML
@@ -28,11 +36,16 @@ class CoinListHelpers {
             for (let newCoin of newCoins) {
                 const buttonStatus = followed.has(newCoin.symbol) ? `bi-moon-stars-fill` : `bi-moon`;
                 const button = `<i class="like-button bi ${buttonStatus}"></i>`;
-                
+
+                const newCoinPrice = this.currencyFormatter(newCoin.quote.USD.price);
+                const newCoinMarketCap = this.currencyFormatter(newCoin.quote.USD.market_cap);
+                const newCoinVolume = this.currencyFormatter(newCoin.quote.USD.market_cap);
+                const newCoinPercent = newCoin.quote.USD.percent_change_24h.toFixed(2);
+
                 $(".CoinsTable-tbody").append(`
                 <tr class="CoinsTable-row">
                     <th headers="CoinsTable-header2" class="CoinsTable-Row-Header" scope="row">
-                        <div class="CoinsTable-like-button">
+                        <div class="CoinsTable-like-button" data-symbol=${newCoin.symbol}>
                             ${button}
                         </div>
                         <a href="/coins/${newCoin.symbol}">
@@ -43,34 +56,42 @@ class CoinListHelpers {
                         </a>
                     </th>
                     <td headers="CoinsTable-header3" class="CoinsTable-Row-price">
-                        <div class="CoinsTable-text">${newCoin.quote.USD.price}</div>
+                        <div class="CoinsTable-text">${newCoinPrice}</div>
                     </td>
                     <td headers="CoinsTable-header4">
-                        <div class="CoinsTable-text">${newCoin.quote.USD.percent_change_24h}</div>
+                        <div class="CoinsTable-text">${newCoinPercent}%</div>
                     </td>
                     <td headers="CoinsTable-header5">
-                        <div class="CoinsTable-text">${newCoin.quote.USD.market_cap}</div>
+                        <div class="CoinsTable-text">${newCoinMarketCap}</div>
                     </td>
                     <td headers="CoinsTable-header6">
-                        <div class="CoinsTable-text">${newCoin.quote.USD.volume_24h}</div>
+                        <div class="CoinsTable-text">${newCoinVolume}</div>
                     </td>
                 </tr>
                 `)
             }
         }
     }
+
+    currencyFormatter (amount) {
+        let formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',     
+        });
+        return formatter.format(amount); 
+    }
 };
 
 let CoinsHelpers = new CoinListHelpers();
 
-/** Function to add more coins to list
+/** Event Handler to add more coins to list
  * On scroll, if ajaxCoinsLock is false, call getMoreCoins and add 10 coins
  */
  $(".CoinsTable").scroll((evt) => {
     evt.preventDefault();
     const coinsTable = document.querySelector(".CoinsTable");
 
-    if ($(".CoinsTable").scrollTop() + coinsTable.clientHeight >= coinsTable.scrollHeight) {
+    if ($(".CoinsTable").scrollTop() + coinsTable.clientHeight >= coinsTable.scrollHeight - 50) {
         if (CoinsHelpers.ajaxCoinsLock === true) {
             CoinsHelpers.ajaxCoinsLock = false;
             //return;
@@ -80,4 +101,15 @@ let CoinsHelpers = new CoinListHelpers();
             CoinsHelpers.scrollCount += 10; //add 10 to the variable count
         };
     };
+});
+
+/** On Click event handler for like button
+ * handles click on like button (shape of moon). this adds the symbol of the coin to 
+ * user's favorites
+*/
+$(document).on("click", ".like-button", (evt) => {
+    $(evt.target).toggleClass("bi-moon-stars-fill bi-moon");
+    
+    let coinSymbol = $(evt.target).parent().data("symbol");
+    CoinsHelpers.addCoinToFavorite(coinSymbol);
 });
--- static/rocket-news.js
+++ static/rocket-news.js
@@ -1,93 +1,99 @@
-//functions helpers to handle infinite scrolling for news articles
+/** helpers to handle infinite scrolling for News Articles 
+ * OOP class of helpers used to make GET request and add onto the current news HTML list
+ */
+class NewsListHelpers {
+    constructor () {
+        this.symbol = $(".symbol").text();
+        this.paginationCount = 1; //variable for pagination, used for sending requests to external news API
+        this.ajaxLock = false; //variable to instantiate boolean used to pull information while true
+    }
 
-let symbol = $(".symbol").text(); //variable to hold the coin's symbol of the current page
-let paginationCount = 1; //variable for pagination, used for sending requests to external news API
-let ajaxLock = false; //variable to instantiate boolean used to pull information while true
+    /** On Click for like button
+     * handles click on like button (shape of moon). this adds the symbol of the coin to 
+     * user's favorites
+    */
+    async addCoinToFavorite() {
+        const res = await axios.post(`/add/coin/${this.symbol}`);
+        return res;
+    }
 
-/** On Click for like button
+    /** functions to get articles
+     * function to call internal API (which then calls external API) gets news articles using symbol and pagination
+     * then call addArticlesToHtml
+    */
+    async getCoinArticles() {
+        const res = await axios.get(`/api/coin-news/${this.symbol}/${this.paginationCount}`);
+        this.addArticlesToHtml(res);
+        this.ajaxLock = false;
+        this.paginationCount += 1; //increase pagination count for external GET request
+    }
+
+    /** functions to add new articles to HTML
+     * for each new article, add news information to HTML 
+    */
+    addArticlesToHtml(res) {
+        let article_info = res.data.articles; //instantiate articles to article_info
+        for (let article of article_info) {
+            $(".article-section").append(`
+            <div class="card">
+            <div class="card-body">
+                <div class="article-image-title">
+                    <div class="article-image">
+                        <a href="${article.url}">
+                            <img src="${article.image}" alt="article-image"
+                                onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWfbGNKZyGwrxymaoronVG972LJ0Nd3OspQ&usqp=CAU';">
+                        </a>
+                    </div>
+                    <div class="article-title text-decoration-none">
+                        <a href="${article.url}">
+                            <h4>${article.title}</h4>
+                        </a>
+                    </div>
+                </div>
+                <div class="article-info">
+                    <div class="article-source text-decoration-none">
+                        <a href="${article.url}">${article.source}</a>
+                    </div>
+                    <div class="article-author">
+                        <span>Created By: ${article.source}</span>
+                    </div>
+                    <div class="article-description">
+                        <span>${article.description}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `)
+        }
+    }
+};
+
+let NewsHelpers = new NewsListHelpers();
+
+/** Event Handler to add more articles to list
+ * On scroll, if ajaxCoinsLock is false, call getCoinArticles and add additional articles
+ */
+ $(window).scroll((evt) => {
+    evt.preventDefault()
+
+    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+        if (NewsHelpers.ajaxLock === true) {
+            NewsHelpers.ajaxLock = false;
+        } else {
+            NewsHelpers.ajaxLock = true; //set ajaxCoinsLock to true
+            NewsHelpers.getCoinArticles(); //function to call more articles
+        }
+    }
+});
+
+/** On Click event handler for like button
  * handles click on like button (shape of moon). this adds the symbol of the coin to 
  * user's favorites
 */
 $(document).on("click", ".like-button", (evt) => {
     $(evt.target).toggleClass("bi-moon-stars-fill bi-moon");
 
-    let symbol = $(evt.target).parent().data("symbol");
+    let coinSymbol = $(evt.target).parent().data("symbol");
 
-    addCoinToFavorite(symbol);
+    NewsHelpers.addCoinToFavorite(coinSymbol);
 });
-
-/** functions for adding coin to favorite table
- * sends a POST request to backend api for adding or removing coin
-*/
-async function addCoinToFavorite(symbol) {
-    const res = await axios.post(`/add/coin/${symbol}`);
-    return res;
-};
-
-/** functions to handle infinite scrolling for news
- * On scroll down, if ajaxLock is false, increase paginagtion count
- * then get additional articles using coin symbol and pagination
-*/
-$(window).scroll((evt) => {
-    evt.preventDefault()
-    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 40) {
-        if (ajaxLock === true) {
-            ajaxLock = false;
-            return
-        } else {
-            ajaxLock = true; //set ajaxCoinsLock to true
-            paginationCount += 1; //increase pagination count for external GET request
-
-            getCoinArticles(symbol, paginationCount); //function to call more articles
-        }
-    }
-});
-
-/** functions to get articles
- * function to call internal API (which then calls external API) gets news articles using symbol and pagination
- * then call addArticlesToHtml
-*/
-async function getCoinArticles(symbol, count) {
-    const res = await axios.get(`/api/coin-news/${symbol}/${count}`);
-    addArticlesToHtml(res);
-    ajaxLock = false;
-};
-
-/** functions to add new articles to HTML
- * for each new article, add news information to HTML 
-*/
-function addArticlesToHtml(res) {
-    article_info = res.data.articles; //instantiate articles to article_info
-    for (let article of article_info) {
-        $(".article-section").append(`
-        <div class="card">
-        <div class="card-body">
-            <div class="article-image-title">
-                <div class="article-image">
-                    <a href="${article.url}">
-                        <img src="${article.image}" alt="article-image"
-                            onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWfbGNKZyGwrxymaoronVG972LJ0Nd3OspQ&usqp=CAU';">
-                    </a>
-                </div>
-                <div class="article-title text-decoration-none">
-                    <a href="${article.url}">
-                        <h4>${article.title}</h4>
-                    </a>
-                </div>
-            </div>
-            <div class="article-info">
-                <div class="article-source text-decoration-none">
-                    <a href="${article.url}">${article.source}</a>
-                </div>
-                <div class="article-author">
-                    <span>Created By: ${article.source}</span>
-                </div>
-                <div class="article-description">
-                    <span>${article.description}</span>
-                </div>
-            </div>
-        </div>
-    </div>
-    `);
-    };
-};
