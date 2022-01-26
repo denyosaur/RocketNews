6b98c8ee2b19616af84c0dd2b9d57aa5003c5c33
denyosaur <operationdaniel@gmail.com>
2022-01-25 23:14:19 -0800

    tests

--- test_rocketnews.py
+++ test_rocketnews.py
@@ -3,8 +3,8 @@ from unittest import TestCase
 from flask import session
 from form import UserRegistration, UserProfile, LoginForm
 
-##app.config['TESTING'] = True
-##app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class RocketNewsTestCase(TestCase):
     def setUp(self):
@@ -16,39 +16,39 @@ class RocketNewsTestCase(TestCase):
         db.session.remove()
         db.drop_all()
 
-    # def test_homepage_not_logged_in(self):
-    #     with app.test_client() as client:
-    #         res = client.get('/')
-    #         html = res.get_data(as_text=True)
+    def test_homepage_not_logged_in(self):
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
 
-    #         self.assertEqual(res.status_code, 302)
-    #         self.assertEqual(res.location, 'http://localhost/login')
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, 'http://localhost/login')
             
-    # def test_login_page(self):
-    #     with app.test_client() as client:
-    #         res = client.get('/login')
-    #         html = res.get_data(as_text=True)
+    def test_login_page(self):
+        with app.test_client() as client:
+            res = client.get('/login')
+            html = res.get_data(as_text=True)
 
-    #         self.assertEqual(res.status_code, 200)
-    #         self.assertIn('<h1>Login</h1>', html)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Login</h1>', html)
     
-    # def test_register_page(self):
-    #     with app.test_client() as client:
-    #         res = client.get('/register')
-    #         html = res.get_data(as_text=True)
+    def test_register_page(self):
+        with app.test_client() as client:
+            res = client.get('/register')
+            html = res.get_data(as_text=True)
 
-    #         self.assertEqual(res.status_code, 200)
-    #         self.assertIn('<h1>Register</h1>', html)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Register</h1>', html)
 
-    # def test_expects_to_instantiate_new_form(self):
-    #     with app.test_request_context('/register'):
-    #         form = UserRegistration()
-    #     self.assertIsInstance(form, UserRegistration)
+    def test_expects_to_instantiate_new_form(self):
+        with app.test_request_context('/register'):
+            form = UserRegistration()
+        self.assertIsInstance(form, UserRegistration)
 
 
-    def test_expects_to_register_user(self):
+    def test_expects_new_register_and_login(self):
         app.config['WTF_CSRF_ENABLED'] = False
-
+        ##Register new account
         user_data = {
                 'username':'testuser4', 
                 'password':'testpass', 
@@ -58,6 +58,8 @@ class RocketNewsTestCase(TestCase):
             '/register',
             data = user_data)
         self.assertEqual(res.status_code, 302)
+
+        ##Login to new account
 
     def test_expects_new_register_to_redirect(self):
         app.config['WTF_CSRF_ENABLED'] = False
